57d4634aeb8fa5a3db8559faba605dd133ac8a03
Louis Ruocco <louisruocco1@gmail.com>
2023-03-05 20:44:04 +0000

    able to add IP addresses and query them against existing records to stop duplicate entries

--- script.ps1
+++ script.ps1
@@ -2,6 +2,22 @@ install-module mdbc
 import-module mdbc
 connect-mdbc . ping ips
 
-function addIP {
+function addRecord($ip, $name) {
     $ip = Read-Host -Prompt "Please Type IP Address here"
+    $name = Read-Host -Prompt "Please Type the name of the device this IP address relates to"
+    action
 }
+
+function action {
+    $getIP = get-mdbcdata @{ip = $ip}
+    if($getIP.ip -eq $ip){
+        return write-output "Device Already Exists! Please type 'addRecord' to try again"
+    } else {
+        @{ip = $ip; name = $name} | add-mdbcdata
+        write-output "Device: $name with $ip added Successfully!"
+        start-sleep 1
+        write-output "Please type 'addRecord' to add another IP Address"
+    }
+}   
+
+addRecord -ip (action)
