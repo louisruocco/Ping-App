4d01f905d5d0b87d777fb6a310e05fc68314a881
Louis Ruocco <louisruocco1@gmail.com>
2023-03-06 21:00:31 +0000

    removed params from addIP function due to redundancy

--- script.ps1
+++ script.ps1
@@ -2,22 +2,22 @@ install-module mdbc
 import-module mdbc
 connect-mdbc . ping ips
 function addIP {
-    param (
-        [string]$ipPrompt, 
-        [string]$namePrompt
-    )
-
-    $ip = Read-Host -Prompt $ipPrompt
+    $ip = Read-Host -Prompt "Please Add IP Address Here"
     $checkIP = get-mdbcdata @{ip = $ip}
     if($checkIP.ip -eq $ip){
         write-host "Record already exists!"
         start-sleep 1
         return
     } else {
-        $name = Read-Host -Prompt $namePrompt
+        $name = Read-Host -Prompt "Please Add the name of the device here"
         @{ip = $ip; name = $name; date = Get-Date} | add-mdbcdata
         write-host "$ip successfully added! Please type 'addIP' to add another IP address or 'showRecords' to see what IP addresses are already inputted"
     }
 }
 
-addIP "Please Add IP Address Here" "Please Add the name of the device here"
+function showRecord {
+    write-host "Here is a list of all the existing IPs inputted and pingable:"
+    Get-MdbcData
+}
+
+addIP
