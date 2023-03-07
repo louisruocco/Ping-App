a9c37d59563ae4fb097a23954553a0fa8e06d682
Louis Ruocco <louisruocco1@gmail.com>
2023-03-07 13:47:49 +0000

    started ping script and modified some console responses

--- script.ps1
+++ script.ps1
@@ -5,7 +5,14 @@ if(get-module mdbc){
     Write-Host "Type 'addIP to add an IP address or 'showRecords to show all existing IPs"
 } else {
     install-module mdbc
+    write-host "Installing mdbc module..."
+    Write-Host "Type 'addIP to add an IP address or 'showRecords to show all existing IPs"
 }
+
+$pingIPs = get-mdbcdata
+write-host "Pinging IPs..."
+start-sleep 1
+write-host $pingIPs
 
 function addIP {
     $ip = Read-Host -Prompt "Please Add IP Address Here"
@@ -17,7 +24,9 @@ function addIP {
     } else {
         $name = Read-Host -Prompt "Please Add the name of the device here"
         @{ip = $ip; name = $name; date = Get-Date} | add-mdbcdata
-        write-host "$ip successfully added! Please type 'addIP' to add another IP address or 'showRecords' to see what IP addresses are already inputted"
+        write-host "$ip successfully added!"
+        start-sleep 1
+        write-host "Please type 'addIP' to add another IP address or 'showRecords' to see what IP addresses are already inputted"
     }
 }
 
@@ -30,8 +39,8 @@ function showRecords {
 
     write-host "Here is a list of all the existing IPs inputted and pingable:"
     if($getIPs -eq $null){
-        start-sleep 1
-        write-host "No IPs added! please type 'addIP' to add an IP address"
+        start-sleep 1s
+        return write-host "No IPs added! please type 'addIP' to add an IP address"
     } else {
         $getIPs
     }
