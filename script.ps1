411d10f5403246d626a68b8e85d3fcea8fe94c63
Louis Ruocco <louisruocco1@gmail.com>
2023-03-07 15:14:39 +0000

    added schedule to ping function

--- script.ps1
+++ script.ps1
@@ -1,5 +1,18 @@
 import-module mdbc
 connect-mdbc . ping ips
+function runPing {
+    $ips = Get-MdbcData
+    foreach($ip in $ips.ip){
+        Connect-Mdbc . ping logs
+        $ping = Test-Connection $ip -count 1 -quiet
+        @{ip = $ip; date = Get-Date ; status = $ping} | Add-MdbcData
+    }
+}
+
+while (1 -eq 1){
+    runPing
+    start-sleep -seconds 300
+}
 
 if(get-module mdbc){
     Write-Host "Type 'addIP to add an IP address or 'showRecords to show all existing IPs"
